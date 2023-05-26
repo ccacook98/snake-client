469fc1bf64674ed43766583e9315cafb447d7047
ccacook98 <caleb_cook@outlook.com>
2023-05-26 03:01:19 +0000

    play.js: Added initial user input handling.

--- play.js
+++ play.js
@@ -3,3 +3,15 @@ const {connect} = require("./client.js");
 
 console.log("Connecting ...");
 connect();
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+
